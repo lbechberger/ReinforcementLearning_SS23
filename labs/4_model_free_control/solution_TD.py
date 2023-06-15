fe137c3cc37bda661a18c3c2d53c18713cd9f0e6
Lucas Bechberger <academia@lucas-bechberger.de>
2023-06-15 18:04:48 +0200

    add solution for tasks 2 and 3 of lab 4 (MC control)

--- labs/4_model_free_control/solution_TD.py
+++ labs/4_model_free_control/solution_TD.py
@@ -8,6 +8,8 @@ random.seed(0)
 np.random.seed(0)
 env.reset(seed=0)
 
+alpha = 0.5
+
 print("## Frozen Lake ##")
 print("Start state:")
 print(env.render())
@@ -19,50 +21,41 @@ no_actions = env.action_space.n
 def play_episode(q_values, epsilon):
 
     state, _ = env.reset()
+    action = choose_action(q_values, state, epsilon)
     done = False
     r_s = []
-    s_a = []
     while not done:
-        # use q-values to implement epsilon-greedy
-        action = choose_action(q_values, state, epsilon)
+        next_state, reward, done, _, _ = env.step(action)
+        next_action = choose_action(q_values, next_state, epsilon)
 
-        s_a.append((state, action))
-        state, reward, done, _, _ = env.step(action)
+        q_values[state][action] += alpha * (reward + q_values[next_state][next_action] - q_values[state][action])
+        state = next_state
+        action = next_action
+
         r_s.append(reward)
-    return s_a, r_s
+    return r_s
 
 
 def choose_action(q_values, state, epsilon):
-
     if random.random() > epsilon:
-        # use optimal action
         max_indices = [i for i, v in enumerate(q_values[state]) if v == max(q_values[state])]
         return random.choice(max_indices)
     else:
-        # explore randomly
         return random.randint(0, 3)
 
 
 def main():
     no_episodes = 1000
     epsilons = [0.01, 0.1, 0.5, 1.0]
-
     plot_data = []
 
     for e in epsilons:
         q_values = np.zeros((no_states, no_actions))
-        q_counter = np.zeros((no_states, no_actions))
 
         rewards = []
         for j in range(0, no_episodes):
-            s, r = play_episode(q_values, e)
+            r = play_episode(q_values, e)
             rewards.append(sum(r))
-
-            # update q-values with MC-prediction
-            for i, (s, a) in enumerate(s):
-                return_i = sum(r[i:])
-                q_counter[s][a] += 1
-                q_values[s][a] += (1 / q_counter[s][a]) * (return_i - q_values[s][a])
 
         plot_data.append(np.cumsum(rewards))
 
